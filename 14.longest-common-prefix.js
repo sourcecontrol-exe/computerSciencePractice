3a7a4eb6d537377eff4cb298696d4e7c1ef4fb71
swetabh <swetabh.subham@gmail.com>
2023-10-17 02:57:24 +0530

    todo

---
+++ 14.longest-common-prefix.js
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode id=14 lang=javascript
+ *
+ * [14] Longest Common Prefix
+ */
+
+// @lc code=start
+/**
+ * @param {string[]} strs
+ * @return {string}
+ */
+var longestCommonPrefix = function(strs) {
+	let result = '';
+	for(let i =0; i < strs.length - 1 ; i++){
+		if(result)
+	}
+};
+console.log(longestCommonPrefix(["flower","flow","flight"]));
+// @lc code=end
+
